06d6c5ffbb98f85ce0fe3a00e992cb463dac894d
guillenavas <guillenavas10@gmail.com>
2024-05-05 04:24:42 +0200

    guille empeiza profileinformation

--- ProfileInformation.js
+++ ProfileInformation.js
@@ -1,17 +1,105 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, ImageBackground } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
+import { supabase } from './src/supabase';
 
-const HelloScreen = ({ navigation }) => {
+export default function RegisterScreen({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleRegister = async () => {
+    try {
+      // Validar que todos los campos estén completos
+      if (!email || !password || !confirmPassword) {
+        throw new Error('Please fill in all fields');
+      }
+
+      // Validar que el email sea una dirección de email válida
+      if (!validateEmail(email)) {
+        throw new Error('Please enter a valid email address');
+      }
+
+      // Validar que la contraseña tenga al menos 6 caracteres
+      if (password.length < 6) {
+        throw new Error('Password must be at least 6 characters long');
+      }
+
+      // Validar que las contraseñas coincidan
+      if (password !== confirmPassword) {
+        throw new Error('Passwords do not match');
+      }
+
+      const { user, error } = await supabase.auth.signUp({
+        email,
+        password,
+      });
+
+      // Implementar lógica para registrar al usuario
+      console.log('Email:', email);
+      console.log('Password:', password);
+      console.log('Confirm Password:', confirmPassword);
+
+      // Aquí puedes agregar la lógica para registrar al usuario en la base de datos
+
+      // Navegar a la siguiente pantalla después del registro
+      if (!error) navigation.navigate('Preferences');
+      else throw new Error(error.message);
+    } catch (error) {
+      alert(error.message);
+      console.error('Error registering user:', error.message);
+    }
+  };
+
+  // Función para validar una dirección de email
+  const validateEmail = (email) => {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  };
+
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
-        <AntDesign name="arrowleft" size={24} color="white" />
-      </TouchableOpacity>
-      <Text style={styles.text}>Hola</Text>
-    </View>
+    <ImageBackground source={require('./assets/background_pattern.png')} style={styles.backgroundImage}>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
+          <AntDesign name="arrowleft" size={24} color="white" />
+        </TouchableOpacity>
+        <Text style={styles.title}>Create your account</Text>
+        
+        <TextInput
+          style={{ backgroundColor: 'white', padding: 10, borderRadius: 5, marginTop: 10, width: '45%' }}
+          placeholder="Enter your email"
+          keyboardType="email-address"
+          value={email}
+          onChangeText={text => setEmail(text)}
+        />
+        
+        <TextInput
+          style={{ backgroundColor: 'white', padding: 10, borderRadius: 5, marginTop: 10, width: '45%' }}
+          placeholder="Enter your password"
+          secureTextEntry={true}
+          value={password}
+          onChangeText={text => setPassword(text)}
+        />
+        
+        <TextInput
+          style={{ backgroundColor: 'white', padding: 10, borderRadius: 5, marginTop: 10, width: '45%' }}
+          placeholder="Confirm your password"
+          secureTextEntry={true}
+          value={confirmPassword}
+          onChangeText={text => setConfirmPassword(text)}
+        />
+        <View style={styles.separator}></View>
+        <TouchableOpacity
+          style={[styles.button, { backgroundColor: '#042f83' }]}
+          onPress={handleRegister}
+        >
+          <AntDesign name="adduser" size={24} color="white" />
+          <Text style={styles.buttonText}>Register</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -19,16 +107,49 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  text: {
-    fontSize: 24,
+  title: {
+    fontFamily: 'nombre-de-la-fuente', // Reemplaza 'nombre-de-la-fuente' con el nombre de la fuente deseada
+    fontSize: 35,
     fontWeight: 'bold',
+    fontFamily: 'sans-serif',
+    color: 'white',
+    marginBottom: 50,
+  },
+  input: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
+    width: '80%',
+  },
+  separator: {
+    height: 30, // Espacio entre los botones
+  },
+  button: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '45%',
+    height: 50,
+    marginBottom: 20,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'transparent',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    marginLeft: 10,
   },
   backButton: {
     position: 'absolute',
     top: 30,
     left: 30,
-    zIndex: 1,
+    zIndex: 1, // Para que el botón esté por encima de los otros elementos
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
   },
 });
-
-export default HelloScreen;
